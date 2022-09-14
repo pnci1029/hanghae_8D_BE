4f1edb49618f25a25e5e84a34c5a025d953087eb
ChHong <pnci1029@gmail.com>
2022-09-14 16:52:22 +0900

    [fix] nginx

--- src/main/java/com/example/checkcheck/StompApplication.java
+++ src/main/java/com/example/checkcheck/StompApplication.java
@@ -3,6 +3,7 @@ package com.example.checkcheck;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -13,10 +14,21 @@ import javax.persistence.EntityManager;
 @SpringBootApplication
 public class StompApplication {
 
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml,"
+			+ "/app/config/springboot-webservice/real-application.yml";
+
 	public static void main(String[] args) {
-		SpringApplication.run(StompApplication.class, args);
+		new SpringApplicationBuilder(StompApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
 
 	}
+
+//	public static void main(String[] args) {
+//		SpringApplication.run(StompApplication.class, args);
+//
+//	}
 	@Bean
 	JPAQueryFactory jpaQueryFactory(EntityManager em) {
 		return new JPAQueryFactory(em);
